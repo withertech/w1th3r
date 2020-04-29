f8d4675e47ff42d86a577c582574cac0b6cb3955
Witherking25 <Witherking@withertech.com>
2020-04-29 15:46:34 -0500

    added log

--- w1th3r/ViewController.m
+++ w1th3r/ViewController.m
@@ -8,9 +8,13 @@
 
 #import "ViewController.h"
 #import "exploit.h"
+#import "FdHijacker.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *loggingView;
+@property (strong, nonatomic) FdHijacker *stdoutHijacker;
+@property (strong, nonatomic) FdHijacker *stderrHijacker;
+@property (strong, nonatomic) NSMutableString *contents;
 @end
 
 @implementation ViewController
@@ -18,8 +22,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-}
+    // Add a border around the text view.
+    self.loggingView.layer.borderWidth = 1.0f;
+    self.loggingView.layer.borderColor = [UIColor blackColor].CGColor;
 
+    // Hijack standard out.
+    self.stdoutHijacker = [FdHijacker hijackerWithFd: fileno(stdout)];
+    setbuf (stdout, NULL);
+    self.stdoutHijacker.delegate = self;
+    [self.stdoutHijacker startHijacking];
+    [self.stdoutHijacker startReplicating];
+
+    // Hijack standard error
+    self.stderrHijacker = [FdHijacker hijackerWithFd: fileno(stderr)];
+    setbuf (stderr, NULL);
+    self.stderrHijacker.delegate = self;
+    [self.stderrHijacker startHijacking];
+    [self.stderrHijacker startReplicating];
+
+    self.contents = [NSMutableString string];
+
+    NSLog (@"All Kids Love Log");
+}
+- (void) scrollToEnd {
+    NSRange range = NSMakeRange (self.contents.length, 0);
+    [self.loggingView scrollRangeToVisible: range];
+} // scrollToEnd
+
+
+- (void) hijacker: (FdHijacker *) hijacker  gotText: (NSString *) text {
+    if (hijacker == self.stdoutHijacker) [self.contents appendString: @"stdout: "];
+    if (hijacker == self.stderrHijacker) [self.contents appendString: @"stderr: "];
+
+    [self.contents appendString: text];
+    self.loggingView.text = self.contents;
+    [self scrollToEnd];
+}
 - (IBAction)Jailbreak:(UIButton *)sender {
     get_tfp0();
 }
